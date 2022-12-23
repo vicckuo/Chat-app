5430ddb03288936bb6032d7a5886a1b86fa98c9e
Vic Kuo <eun.hru@gmail.com>
2022-12-23 18:38:09 +0800

    commit

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,4 @@
-//部属时 host就直接 //代域名就好 例如 //api.qqmty.cc  不用代端口
-export const host = 'http://localhost:8899';
+export const host = '//chat-app-afi1.onrender.com';
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const verifyEmailRoute = `${host}/api/auth/verifyEmailToken`;
